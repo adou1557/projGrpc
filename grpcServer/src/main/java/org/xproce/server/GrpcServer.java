fea315673e0bb31e757da7299906dcc2323ea078
Your Name <you@example.com>
2024-11-29 10:05:40 +0100

    first commit

---
+++ grpcServer/src/main/java/org/xproce/server/GrpcServer.java
@@ -0,0 +1,14 @@
+package org.xproce.server;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import org.xproce.service.BankGrpcService;
+import java.io.IOException;
+public class GrpcServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(5555)
+
+                .addService(new BankGrpcService())
+
+                .build(); server.start(); server.awaitTermination();
+    }
+}
